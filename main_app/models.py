626046f1c5563d992f17f66a2f304025333babb6
RACLesty <ztos@msn.com>
2024-03-12 13:18:09 -0400

    Part 2 Update

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Finch(models.Model):
+   breed = models.CharField()
+   habitat = models.TextField()
+   description = models.TextField()
+   population = models.IntegerField()
+
+   def __str__(self):
+      return self.breed
